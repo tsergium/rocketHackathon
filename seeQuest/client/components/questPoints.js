29affdcdf511f1a0bdae327108334d5e74f77de8
Ana-Maria Kadar <anamaria.kadar@rocket-internet.de>
2017-04-01 21:02:22 +0200

    Map demo quest inactive state added

--- seeQuest/client/components/questPoints.js
+++ seeQuest/client/components/questPoints.js
@@ -4,7 +4,7 @@ class QuestPoints extends Component {
     render() {
         return (
             <ul className="questPoints">
-                <li><p className="text">45min ago</p></li>
+                <li className="active"><p className="text">45min ago</p></li>
                 <li><p className="text">30min ago</p></li>
                 <li><p className="text">20min ago</p></li>
             </ul>
