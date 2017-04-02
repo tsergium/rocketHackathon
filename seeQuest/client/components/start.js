1128826fd508c97c0b473a6d125e909611723225
Sergiu Tomsa <sergiu.tomsa@shopwings.com>
2017-04-02 11:18:56 +0200

    Finished start screen

--- seeQuest/client/components/start.js
+++ seeQuest/client/components/start.js
@@ -22,16 +22,16 @@ class Start extends Component {
         return(
             <div>
             <div className="startScreen" onClick={this.openModal.bind(this)}>
-                <a className="to-map-button" href="#" onClick={this.redirectTo.bind(this)}></a>
             </div>
             <div className="modal-windows">
                 {this.state.modalActive && (
                     <div className='modal-dialog'>
-                        <img className="cat-modal-details" src="/cat_modal_details.png" />
+                        <img className="cat-modal-details" src="/questSidebarHeaderCat.png" />
                         <a className="modal-close" title='Close' onClick={this.closeModal.bind(this)}>X</a>
                         <p>Name: Pint</p>
                         <p>4 years old, boy cat. He might be close to good craft beer places because he loves beer!</p>
                         <p>(60min)</p>
+                        <a className="to-map-button" href="#" onClick={this.redirectTo.bind(this)}></a>
                     </div>
                 )}
             </div>
