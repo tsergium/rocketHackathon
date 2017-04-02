de8c97b35c54217c7c7f97c8c8b44c1c4a5635e3
Ana-Maria Kadar <anamaria.kadar@rocket-internet.de>
2017-04-02 11:31:08 +0200

    map design changes

--- seeQuest/client/components/map/mapcontainer.js
+++ seeQuest/client/components/map/mapcontainer.js
@@ -176,15 +176,15 @@ function handleMapOptions() {
 function handleOnReady(name) {
     GoogleMaps.ready(name, map => {
         const imagePerson = {
-            url: 'http://localhost:3000/icPerson.png',// image is in root directory of meteor project.
+            url: '/icPerson.png',// image is in root directory of meteor project.
         };
 
         const image = {
-            url: 'http://localhost:3000/greenMarker.png',// image is in root directory of meteor project.
+            url: '/greenMarker.png',// image is in root directory of meteor project.
         };
 
         const imageFinal = {
-            url: 'http://localhost:3000/catMarkerGreen.png',// image is in root directory of meteor project.
+            url: '/catMarkerGreen.png',// image is in root directory of meteor project.
         };
 
         const sidebarQuestPoint = document.querySelectorAll('.questPoints li');
